aa8932e4c25463210b6b2a46e99555ce707a8333
Doan Bui <bxdoan93@gmail.com>
2019-09-02 14:00:09 +0700

    sql practice

---
+++ sql/hkr/placements.sql
@@ -0,0 +1,24 @@
+-- You are given three tables: Students, Friends and Packages. Students 
+-- contains two columns: ID and Name. Friends contains two columns: ID and
+--  Friend_ID (ID of the ONLY best friend). Packages contains two columns:
+--  ID and Salary (offered salary in $ thousands per month).
+--
+-- Sample Output
+-- Samantha
+-- Julia
+-- Scarlet
+--
+-- Explanation
+--
+-- See the following table:
+-- Now,
+--
+-- Samantha's best friend got offered a higher salary than her at 11.55
+-- Julia's best friend got offered a higher salary than her at 12.12
+-- Scarlet's best friend got offered a higher salary than her at 15.2
+-- Ashley's best friend did NOT get offered a higher salary than her
+-- The name output, when ordered by the salary offered to their friends, will be:
+--
+-- Samantha
+-- Julia
+-- Scarlet
